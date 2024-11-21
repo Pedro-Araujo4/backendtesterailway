ac7c2ca95dda86356bbacfa40134a8724d959479
Pedro Araujo <pa298571@gmail.com>
2024-11-21 15:29:08 -0300

    commit-final

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const nomes = [
+  {
+    nome: "Júlio", 
+    cpf: "32423423-212",
+    deploy: "sucesso!",
+  },
+  {
+    nome: "Pedro",
+    cpf: "32423423-212",
+    deploy: "sucesso!",
+  }
+]
 
 app.get('/', function (req, res) {
-  res.status(200).send('Deploy backend bem-sucedido!')
+  res.status(200).json(nomes)
 })
 
 app.listen(process.env.PORT || 3000)
